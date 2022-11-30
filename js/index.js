40d3fe0bbe1d94bbbec25d7bf5824f68008d6a4d
DashkevichAS <antonsnov0604@gmail.com>
2022-11-30 10:35:11 +0300

    add render catalog items

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const product = {
+const burgerMax = {
   title: 'Бургер Макс',
   price: 10000,
   weight: 5000,
@@ -17,35 +17,69 @@ const product = {
 const modalProduct = document.querySelector('.modal_product');
 const catalogList = document.querySelector('.catalog__list');
 
-const modalProductTitle = document.querySelector('.modal-product__title');
-const modalProductImage = document.querySelector('.modal-product__image');
-const modalProductDescription = document.querySelector(
-  '.modal-product__description',
-);
-const ingredientsList = document.querySelector('.ingredients__list');
-const ingredientsItem = document.querySelector('.ingredients__item');
-const ingredientsCalories = document.querySelector('.ingredients__calories');
-const modalProductPriceCount = document.querySelector(
-  '.modal-product__price-count',
-);
+const openModal = (product) => {
+  const modalProductTitle = document.querySelector('.modal-product__title');
+  const modalProductImage = document.querySelector('.modal-product__image');
+  const modalProductDescription = document.querySelector(
+    '.modal-product__description',
+  );
+  const ingredientsList = document.querySelector('.ingredients__list');
+  const ingredientsCalories = document.querySelector('.ingredients__calories');
+  const modalProductPriceCount = document.querySelector(
+    '.modal-product__price-count',
+  );
 
-modalProductTitle.textContent = product.title;
-modalProductImage.src = product.image;
-modalProductDescription.textContent = product.description;
-ingredientsCalories.textContent = `${product.weight}г, ккал ${product.calories}`;
-ingredientsList.textContent = '';
+  modalProductTitle.textContent = product.title;
+  modalProductImage.src = product.image;
+  modalProductDescription.textContent = product.description;
+  ingredientsCalories.textContent = `${product.weight}г, ккал ${product.calories}`;
+  ingredientsList.textContent = '';
+  modalProductPriceCount.textContent = product.price;
+  const ingredientsListItems = product.ingridients.map((item) => {
+    const li = document.createElement('li');
+    li.classList.add('ingredients__item');
+    li.textContent = item;
+    return li;
+  });
+  ingredientsList.append(...ingredientsListItems);
 
-const ingredientsListItems = product.ingridients.map((item) => {
+  modalProduct.classList.add('modal_open');
+};
+
+const createCardProduct = (product) => {
   const li = document.createElement('li');
-  li.classList.add('ingredients__item');
-  li.textContent = item;
+  li.classList.add('catalog__item');
+
+  li.innerHTML = `
+    <article class="product">
+      <img
+        class="product__image"
+        src=${product.image}
+        alt=${product.title}
+      />
+
+      <p class="product__price">
+        ${product.price}<span class="currency">₽</span>
+      </p>
+
+      <h3 class="product__title">
+        <button class="product__detail">${product.title}</button>
+      </h3>
+
+      <p class="product__weight">${product.weight}г</p>
+
+      <button class="product__add">Добавить</button>
+  </article>
+  `;
   return li;
-});
-ingredientsList.append(...ingredientsListItems);
+};
+
+catalogList.textContent = '';
+catalogList.append(createCardProduct(burgerMax));
 
 catalogList.addEventListener('click', ({ target }) => {
   if (target.closest('.product__detail') || target.closest('.product__image')) {
-    modalProduct.classList.add('modal_open');
+    openModal(burgerMax);
   }
 });
 
